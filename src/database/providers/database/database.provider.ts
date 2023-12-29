06dae36b684ffb03ed482a0936b035baa455d1d1
danlowo <danielolulowo3@gmail.com>
2023-12-29 17:55:11 +0100

    fix db issue on cyclic

--- src/database/providers/database/database.provider.ts
+++ src/database/providers/database/database.provider.ts
@@ -1,5 +1,6 @@
 import { Sequelize } from 'sequelize-typescript';
 import {TodoEntity} from "../../../todos/entity/todo.entity";
+import {dbURL} from "../../database.module";
 
 export const databaseProviders = [
     {
@@ -7,7 +8,7 @@ export const databaseProviders = [
         useFactory: async () => {
             const sequelize = new Sequelize({
                 dialect: 'sqlite',
-                storage: "https://firebasestorage.googleapis.com/v0/b/wendor-dc3ab.appspot.com/o/database.sqlite?alt=media&token=d53e49c6-abdd-4d40-9d1f-11dcf09644e3"
+                storage: dbURL.prod2
             });
             sequelize.addModels([TodoEntity]);
             await sequelize.sync();
