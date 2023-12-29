d9361cab78dc3e6fe710414fc16b4f8748abdf20
danlowo <danielolulowo3@gmail.com>
2023-12-29 16:37:53 +0100

    add todo module and database module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TodosModule } from './todos/todos.module';
+import { DatabaseModule } from './database/database.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TodosModule, DatabaseModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
