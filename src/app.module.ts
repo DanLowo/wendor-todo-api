5d9fed045ea211dc98ec014a8aa8e84dfbfa0d6c
danlowo <danielolulowo3@gmail.com>
2023-12-30 03:43:13 +0100

    setup nest for env configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TodosModule } from './todos/todos.module';
 import { DatabaseModule } from './database/database.module';
-
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TodosModule, DatabaseModule],
+  imports: [TodosModule, DatabaseModule, ConfigModule.forRoot()],
   controllers: [],
   providers: [],
 })
